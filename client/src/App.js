0c882d0b70f3825b0c1c6afb7bca19bfbd0bd2ad
Raph Lee <leer@dickinson.edu>
2020-06-23 16:13:23 +0900

    Refactored from classes to function

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,29 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, {useEffect, useState} from "react";
+import MainProfile from "./components/MainProfile"
+
 import "./App.css";
+function App() {
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: "" };
-    }
-
-    callAPI() {
+    const [apiResponse, setApiResponse] = useState(0);
+    
+    useEffect(()=>{
+        callAPI();
+    })
+    const callAPI =(props)=>{
         fetch("http://localhost:9000/testAPI")
+        // .then(res=>console.log(res.text()))
             .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
+            .then(res => setApiResponse(res))
             .catch(err => err);
     }
 
-    componentDidMount() {
-        this.callAPI();
-    }
+    return (
+        <div className="App">
+            <MainProfile/>
+            <p className="App-intro">{apiResponse}</p>
+        </div>
+    );
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to your new Project!</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
-            </div>
-        );
-    }
 }
 
 export default App;
---
+++ client/src/components/MainProfile.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+
+const MainProfile = () => {
+    return(
+        <div>
+            <button>hi</button>
+        </div>
+    )
+}
+
+export default MainProfile;
