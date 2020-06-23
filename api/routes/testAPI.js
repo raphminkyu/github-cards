61b7beba3d7a1c7a0a0c7ed3fdc061d7b44e7f9f
Raph Lee <leer@dickinson.edu>
2020-06-23 18:04:54 +0900

    Frontend 1st Draft

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const fetch = require('node-fetch');
 router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+
+    // const gitAPI = "https://api.github.com/users/raphminkyu"
+    // const callFetch = fetch(gitAPI)
+    //     .then(res=>res.text())
+    //     // .then(res=>console.log(res.text()))
+    //         // .then(res => console.log(res))
+    //         // .then(res => setApiResponse(res))
+    //         .catch(err => err);
+
+
+
+    // res.send(callFetch);
+    res.send("ss");
 });
 
 module.exports = router;
--- client/src/components/MainProfile.jsx
+++ client/src/components/MainProfile.jsx
@@ -1,11 +1,120 @@
 import React, { useState } from 'react';
+import { Avatar, useRadioGroup } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from '@material-ui/core/Paper';
+import { useEffect } from 'react';
 
+const useStyles = makeStyles((theme) => ({
+    main:{
+    
+        display: "flex",
+        flexDirection: "column",
+        textAlign:"center",
+        width:"200px",
+        margin:"0 auto",
+        alignItems:"center"
+    
 
+    },
+    avatar: {
+      width: theme.spacing(14),
+      height: theme.spacing(14),
+      borderRadius: 30,
+     padding:0,
+     margin:0
+    },
+    info:{
+        display:"flex",
+        flexDirection:"row",
+    },
+    infoItem:{
+        display:"flex",
+        flexDirection:"column",
+        borderStyle:"solid",
+        borderWidth:"0.5px",
+
+        width:"66px"
+    }
+  }));
 const MainProfile = () => {
+
+    const classes = useStyles();
+
+    const [user, setUser]= useState([]);
+
+    useEffect(()=>{
+        fetch("https://api.github.com/users/raphminkyu")
+        .then(res=>res.json())
+        
+        .then(res=>setUser(res))
+        // .then(console.log(user))
+        // .then( res=>console.log(res))
+        // 
+        
+       
+       
+    }, [])
+    
+
+
+    {console.log(user)}
+    {console.log(user.length)}
+
     return(
-        <div>
-            <button>hi</button>
-        </div>
+            <div>
+                {user && user.length!=0 ? (
+
+                    <Paper id = "main" className = {classes.main}>
+                    {/* {console.log(gitAPI)} */}
+            
+                    <Avatar
+                    className={classes.avatar}
+                    //   style={{alignSelf: 'center'}}
+                        src = {user.avatar_url}
+                    />
+                    <div>
+                        {user.name}
+                    </div>
+                    <div>
+                        {user.login}
+                    </div>
+
+                    <div id="info" className={classes.info}>
+                        <div className={classes.infoItem}>
+                            <div>
+                                {user.public_repos}
+                            </div>
+                            <div>
+                                Repo
+                            </div>
+                        </div>
+                        <div className = {classes.infoItem}>
+                            <div>
+                              {user.public_gists}
+                            </div>
+                            <div>
+                                Gist
+                            </div>
+                        </div>
+                        <div className = {classes.infoItem}>
+                            <div>
+                                {user.followers}
+                            </div>
+                            <div>
+                                Followers
+                            </div>
+                        </div>
+                    </div>
+            
+                </Paper>
+                ): (
+                    <div>
+                        Loading
+                    </div>
+                )}
+            </div>
+
+       
     )
 }
 
